e85208c3b7daecd12508c9dd54423af339e5303f
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:56:27 +0700

    add tugas 22 JS

---
+++ tugas22.js
@@ -0,0 +1,10 @@
+function tugas22() {
+
+
+  var data = ["Saya","ingin","belajar","bersama"]
+  data.forEach(function (item, index){
+    console.log("item : ",item,"index ke ",index);
+  })
+}
+
+tugas22();
